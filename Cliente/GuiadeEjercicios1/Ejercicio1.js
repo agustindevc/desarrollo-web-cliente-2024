77246f3261c8624bcf4e403858c65e65d4dddb15
Agustin <agustind1212gmail.com>
2024-10-09 14:36:04 +0200

    Clase 09/10/24

--- Cliente/GuiadeEjercicios1/Ejercicio1.js
+++
@@ -1,7 +0,0 @@
-/*Añadir a una página HTML un script que muestre un alert de la
-siguiente forma:
-a) Añadimos el alert de forma interna sin función.
-b) Añadimos el alert en un evento en un botón.
-c) Igual que el punto anterior, pero usamos una función.
-d) Igual, pero la función en un archivo externo. */
-
