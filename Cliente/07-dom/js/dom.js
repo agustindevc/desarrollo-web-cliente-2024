3c8efe45eb798a653da1d6de4e0eb2bfccd1e06c
Agustin Diaz <adi0004@alu.medac.es>
2024-10-21 11:26:31 +0200

    clase 21/10/24

---
+++ Cliente/07-dom/js/dom.js
@@ -0,0 +1,9 @@
+function inicioDOM(){
+    console.log("Dom cargado");
+    document.body.style.background = 'red';
+
+    let titulo = document.getElementById("title");
+    console.log(titulo.innerHTML);
+    console.log(titulo);
+    
+}
