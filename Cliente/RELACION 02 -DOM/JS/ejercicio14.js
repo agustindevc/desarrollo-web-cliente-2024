f9d605fd6bb07fba6e5cdcae09e9f3f5d109ea2e
Agustin Diaz <adi0004@alu.medac.es>
2024-11-06 14:35:51 +0100

    avances de ejercicio 14 de la relacion

---
+++ Cliente/RELACION 02 -DOM/JS/ejercicio14.js
@@ -0,0 +1,62 @@
+console.log("cargado");
+let listadoPrincipal = document.querySelectorAll("li");
+
+let pulsado0 = false;
+let pulsado1 = false;
+let pulsado2 = false;
+
+listadoPrincipal[0].onclick = function (){
+    if (!pulsado0){
+        pulsado0 = true;
+
+        if(pulsado1){
+            listadoPrincipal[1].lastChild.remove();
+            pulsado1=false;
+        }
+        if(pulsado2){
+            listadoPrincipal[2].lastChild.remove();
+            pulsado2=false;
+        }
+
+        let datosInicio = ["Portada", "Acerca de mi", "Mis fotoss"];
+        let listaInicio = document.createElement("ul");
+        for(let i = 0; i < datosInicio.length; i++){
+            let item = document.createElement("li");
+            item.innerHTML = datosInicio[i];
+            listaInicio.appendChild(item);
+        }
+        listadoPrincipal[0].appendChild(listaInicio);
+    }
+    else {
+        listadoPrincipal[0].lastChild.remove();
+        pulsado0 = false;
+    }
+};
+
+listadoPrincipal[0].onclick = function (){
+    if (!pulsado0){
+        pulsado0 = true;
+
+        if(pulsado1){
+            listadoPrincipal[1].lastChild.remove();
+            pulsado1=false;
+        }
+        if(pulsado2){
+            listadoPrincipal[2].lastChild.remove();
+            pulsado2=false;
+        }
+
+        let datosInicio = ["Portada", "Acerca de mi", "Mis fotoss"];
+        let listaInicio = document.createElement("ul");
+        for(let i = 0; i < datosInicio.length; i++){
+            let item = document.createElement("li");
+            item.innerHTML = datosInicio[i];
+            listaInicio.appendChild(item);
+        }
+        listadoPrincipal[0].appendChild(listaInicio);
+    }
+    else {
+        listadoPrincipal[0].lastChild.remove();
+        pulsado0 = false;
+    }
+};
