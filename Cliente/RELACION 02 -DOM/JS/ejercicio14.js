7ec2f65abebdea9abaca53b6fa6ef36e04f53cbd
Agustin Diaz <adi0004@alu.medac.es>
2024-11-06 14:40:02 +0100

    ejercicio 14 completo

--- Cliente/RELACION 02 -DOM/JS/ejercicio14.js
+++ Cliente/RELACION 02 -DOM/JS/ejercicio14.js
@@ -33,13 +33,13 @@ listadoPrincipal[0].onclick = function (){
     }
 };
 
-listadoPrincipal[0].onclick = function (){
-    if (!pulsado0){
-        pulsado0 = true;
+listadoPrincipal[1].onclick = function (){
+    if (!pulsado1){
+        pulsado1 = true;
 
-        if(pulsado1){
-            listadoPrincipal[1].lastChild.remove();
-            pulsado1=false;
+        if(pulsado0){
+            listadoPrincipal[0].lastChild.remove();
+            pulsado0=false;
         }
         if(pulsado2){
             listadoPrincipal[2].lastChild.remove();
@@ -53,10 +53,38 @@ listadoPrincipal[0].onclick = function (){
             item.innerHTML = datosInicio[i];
             listaInicio.appendChild(item);
         }
-        listadoPrincipal[0].appendChild(listaInicio);
+        listadoPrincipal[1].appendChild(listaInicio);
     }
     else {
-        listadoPrincipal[0].lastChild.remove();
-        pulsado0 = false;
+        listadoPrincipal[1].lastChild.remove();
+        pulsado1 = false;
+    }
+};
+
+listadoPrincipal[2].onclick = function (){
+    if (!pulsado2){
+        pulsado2 = true;
+
+        if(pulsado0){
+            listadoPrincipal[0].lastChild.remove();
+            pulsado0=false;
+        }
+        if(pulsado1){
+            listadoPrincipal[1].lastChild.remove();
+            pulsado1=false;
+        }
+
+        let datosInicio = ["Portada", "Acerca de mi", "Mis fotoss"];
+        let listaInicio = document.createElement("ul");
+        for(let i = 0; i < datosInicio.length; i++){
+            let item = document.createElement("li");
+            item.innerHTML = datosInicio[i];
+            listaInicio.appendChild(item);
+        }
+        listadoPrincipal[2].appendChild(listaInicio);
+    }
+    else {
+        listadoPrincipal[2].lastChild.remove();
+        pulsado2 = false;
     }
 };
